da9006901670918b3b0fe17220f81a3629766332
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2018-03-09 00:21:34 +1300

    Solved

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project_euler_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var sumSquare = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+               sum += i;
+               sumSquare += (i*i);
+            }
+            Console.WriteLine("The answer is: "+ ((sum*sum) - sumSquare));
+            Console.WriteLine(sumSquare);
+            Console.WriteLine(sum^2);
+        }
+    }
+}
